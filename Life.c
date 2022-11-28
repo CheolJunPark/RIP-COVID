ffe0f1dd67493311435732061e5ded1af15929b8
CheoljunPark <82487512+CheoljunPark@users.noreply.github.com>
2022-11-29 02:20:05 +0900

    Create Life.c

---
+++ Life.c
@@ -0,0 +1,25 @@
+#include "MyHeader.h"
+
+
+//목숨 업데이트
+void updateLife() {
+
+	SetCurrentCursorPos(104, 5);
+	for (int i = 1; i <= 5; i++) {
+		if (i <= game_util.life)
+			RED
+		else
+			WHITE
+			printf(" ♥ ");
+	}
+	WHITE
+}
+//목숨 생성
+void createLife() {
+
+	drawRect(101, 4, 27, 3);
+	game_util.life = 5;
+	game_util.heart = (int*)calloc(5, sizeof(int));
+
+	updateLife();
+}
