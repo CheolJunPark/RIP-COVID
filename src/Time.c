14d09948d24e2dfeca74f7d837493dbaae9f6cf9
CheoljunPark <82487512+CheoljunPark@users.noreply.github.com>
2022-12-05 21:23:08 +0900

    Update Time.c

--- src/Time.c
+++ src/Time.c
@@ -1,10 +1,6 @@
-#include <time.h>
+#include "MyHeader.h"
 
-clock_t start_time;
-clock_t end_time;
-
-
-//시간 업데이트
+// 시간 업데이트
 void updateTime() {
 	SetCurrentCursorPos(110, 1);
 	int min = (int)(end_time - start_time) / CLOCKS_PER_SEC / 60;
@@ -22,7 +18,8 @@ void updateTime() {
 	else
 		printf("%d  :  %d", min, sec);
 }
-//시간 생성
+
+// 시간 생성
 void createTime() {
 	drawRect(101, 0, 27, 3);
 }
