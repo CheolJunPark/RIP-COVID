02d601982521f5e81fa4d3d54bd7079072221631
CheoljunPark <82487512+CheoljunPark@users.noreply.github.com>
2022-12-05 21:22:49 +0900

    Update Shift.c

--- src/Shift.c
+++ src/Shift.c
@@ -1,18 +1,7 @@
 #include "MyHeader.h"
 
-extern humanCurPosX;
-extern humanCurPosY;
-
-extern preX, preY;
-extern boom_flag;
-extern spear_flag;
-extern killed_v;
-extern v_num;
-
-
-
+// 오른쪽
 void ShiftRight(void) {
-
 	if (!DetectCollision(humanCurPosX + 1, humanCurPosY))
 		return;
 	SetCurrentCursorPos(humanCurPosX, humanCurPosY);
@@ -23,11 +12,10 @@ void ShiftRight(void) {
 
 	if (spear_xy.get_check == 1)
 		spear_wear();
-
 }
 
+// 왼쪽
 void ShiftLeft(void) {
-
 	if (!DetectCollision(humanCurPosX - 1, humanCurPosY))
 		return;
 	SetCurrentCursorPos(humanCurPosX, humanCurPosY);
@@ -38,11 +26,10 @@ void ShiftLeft(void) {
 
 	if (spear_xy.get_check == 1)
 		spear_wear();
-
 }
 
+// 위쪽
 void ShiftUp(void) {
-
 	if (!DetectCollision(humanCurPosX, humanCurPosY - 1))
 		return;
 	SetCurrentCursorPos(humanCurPosX, humanCurPosY);
@@ -53,11 +40,10 @@ void ShiftUp(void) {
 
 	if (spear_xy.get_check == 1)
 		spear_wear();
-
 }
 
+// 아래쪽
 void ShiftDown(void) {
-
 	if (!DetectCollision(humanCurPosX, humanCurPosY + 1))
 		return;
 	SetCurrentCursorPos(humanCurPosX, humanCurPosY);
@@ -68,5 +54,4 @@ void ShiftDown(void) {
 
 	if (spear_xy.get_check == 1)
 		spear_wear();
-
 }
